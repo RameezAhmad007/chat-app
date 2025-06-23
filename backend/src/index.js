a34515f9a00fc628743dd1f54721863927c96d30
RameezAhmad007 <rameez007.ra@gmail.com>
2025-06-23 22:35:24 +0500

    fixed the path

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,10 +21,11 @@ app.use(
 );
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  const frontendDistPath = path.join(__dirname, "../../frontend", "dist");
+  app.use(express.static(frontendDistPath));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(frontendDistPath, "index.html"));
   });
 }
 
